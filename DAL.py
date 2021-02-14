717d2852d329cb0e25408e07e9e11c9b60c553a6
Mihir Deshpande <mihirvijdeshpande@gmail.com>
2021-02-14 14:28:47 +0530

    added method to add entry in doctor table

--- DAL.py
+++ DAL.py
@@ -1,4 +1,5 @@
 import DBL
+from uuid import uuid4
 
 #This is the Data Abstraction Layer.
 #This program sits in between database operations and application operations
@@ -6,3 +7,11 @@ import DBL
 #add serial number generater
 
 #add UID generater
+def new_doctor(doctor_name, hour_start, hour_end):
+    doctor_id = str(uuid4())
+    print(doctor_id)
+
+    doctor_isavailable = True
+    DBL.createDoctor(doctor_id ,doctor_name, hour_start, hour_end, doctor_isavailable)
+
+new_doctor('a',9,6)
--- DBL.py
+++ DBL.py
@@ -1,3 +1,10 @@
 from tinydb import Query, TinyDB
 
 #This is the DB Layer. All DB operations will be handled here.
+db = TinyDB('db/db.json')
+table_doctor = db.table('doctor')
+
+def createDoctor(doc_id, doc_name, start, end, isAvailable):
+    table_doctor.insert({'doctor_id': doc_id, 'doctor_name': doc_name, 'hourStart': start, 'hourEnd': end, 'isAvailable': isAvailable})
+    for row in table_doctor:
+        print(row)
